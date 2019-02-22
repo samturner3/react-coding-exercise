c883a891202cb0222db1e77ded22403cd27ca824
samturner3 <samburner3@gmail.com>
2019-02-22 19:35:22 +1100

    implement error message

--- src/components/Events.js
+++ src/components/Events.js
@@ -2,37 +2,46 @@ import React from 'react'
 import injectSheet from 'react-jss'
 import { connect } from 'react-redux'
 import { compose } from 'redux'
-import { getEvents, isEventsReady } from '../selectors'
+import { getEvents, isEventsReady, isEventsError } from '../selectors'
 import Icon from './Icon'
 import titleIcon from '../icons/vivid-angle-top-left.svg'
 import theme from '../style/theme'
 import Event from './Event'
 
-const Events = ({ classes, ready, events }) => (
+const Events = ({ classes, ready, events, error }) => (
   <div className={classes.container}>
     <h3 className={classes.title}>
       <Icon className={classes.titleIcon} symbol={titleIcon} />
       Results{ready && `: ${events.length} events found`}
     </h3>
-    {!ready &&
+    {(!ready && !error) &&
       <div className={classes.loaderIconContainer}>
         <img src='/vividIconLoader.gif' alt='Vivid Logo Icon Loader' />
         <h2>Loading ...</h2>
       </div>
     }
-    {ready && (
+    {(ready && !error) && (
       <div className={classes.tilesWrapper}>
         <div className={classes.tiles}>
           {events.map(event => <Event key={event.id} className={classes.tile} content={event} />)}
         </div>
       </div>
     )}
+    {error &&
+      <div className={classes.loaderIconContainer}>
+        <h1><span className={classes.extraLarge} role='img' aria-label='thinking_face'>🤔</span></h1>
+        <h2>Oops</h2>
+        <h3>Nothing found here</h3>
+        <h3>Please try pressing the back button in your browser</h3>
+      </div>
+    }
   </div>
 )
 
 const mapStateToProps = (state) => ({
   ready: isEventsReady(state),
-  events: getEvents(state)
+  events: getEvents(state),
+  error: isEventsError(state)
 })
 
 export default compose(
@@ -82,6 +91,9 @@ export default compose(
     loaderIconContainer: {
       display: 'grid',
       justifyItems: 'center'
+    },
+    extraLarge: {
+      fontSize: '300%'
     }
   })
 )(Events)
--- src/selectors/events.js
+++ src/selectors/events.js
@@ -2,3 +2,4 @@ export const isEventsBusy = state => !!state.events.busy
 export const getEvents = state => state.events.events
 export const getEventsError = state => state.events.error
 export const isEventsReady = state => !isEventsBusy(state) && getEvents(state).length > 0 && !getEventsError(state)
+export const isEventsError = state => getEventsError(state)
